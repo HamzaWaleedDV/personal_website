a4825ec172cef98aa38bfff32914b19fff5a99ea
Hamza Waleed <hamzawaleed.grant@gmail.com>
2023-08-30 17:17:19 +0300

    add models and templates

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Media(models.Model):
+    logo = models.ImageField()
+    cv = models.FileField(null=True)
+    urls = models.URLField()
+
+
+class Skills(models.Model):
+    my_skills = models.CharField(max_length=50)
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    url = models.URLField()
+    image_for_project = models.ImageField()
