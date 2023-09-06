f0cad86fd5e44ed4b732485c4e4a050f69e8c62f
Hamza Waleed <hamzawaleed.grant@gmail.com>
2023-09-06 16:13:37 +0300

    add contact

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,4 +1,5 @@
 function mail() {
     const message = document.getElementById("body").value;
-    window.open('mailto:learncodeeasily.contact@gmail.com?subject=i want a service&body='+message);
+    const subject = document.getElementById("subject").value;
+    window.open('mailto:learncodeeasily.contact@gmail.com?subject=' + subject + '&body=' + message);
 }
